521ba41ed7c3d0f011568ecfe91b57c5be160f95
Sourish Kundu <sourishkundu07@gmail.com>
2021-05-16 08:12:58 -0500

    Refactored code

--- app.py
+++ app.py
@@ -8,33 +8,30 @@ import numpy as np
 
 app = Flask(__name__)
 
-X, y, fig, ax, w, line, scatter = None, None, None, None, None, None, None
-converged = False
+X, y, w = np.array([]), np.array([]), np.array([])
+fig, ax, line, scatter, gd_line_x = None, None, None, None, None
+df = None
+current_degree = None
 
-current_degree = 1
+is_converged = False
+is_new_point_added = True
+
 epsilon = 1e-4
 
-x_min, x_max = -5, 5
-y_min, y_max = -5, 5
+x_min, x_max = -15, 15
+y_min, y_max = -15, 15
 
-temp = np.linspace(x_min, x_max + 1, 100).reshape(-1, 1)
-line_x = np.hstack([np.ones_like(temp), temp])
+og_line_x_vals = np.linspace(x_min, x_max + 1, 100).reshape(-1, 1)
 
 
 @app.route('/')
 def main():
-    global X, y, fig, ax, w, line
-    X = np.empty((0, 2))
-    y = np.empty((0, 1))
-    w = np.zeros((current_degree + 1, 1))
+    global X, y, fig, ax, w, line, current_degree, df
+    df = pd.DataFrame(columns=['x', 'y'], dtype=np.float)
 
     fig, ax = plt.subplots()
     ax.set(xlim=(x_min, x_max), ylim=(y_min, y_max))
-    ax.tick_params(axis='x', colors='red', direction='out', length=13, width=3)
-    # ax.spines['left'].set_position("zero")
-    # ax.spines['bottom'].set_position("zero")
-
-    line = ax.plot(line_x[:, [1]], line_x @ w, color='b', linewidth=3)
+    line = ax.plot(og_line_x_vals, np.zeros_like(og_line_x_vals), color='b', linewidth=3)
 
     mpld3.plugins.clear(fig)
     plugins.connect(fig, plugins.MousePosition(fontsize=0))
@@ -44,54 +41,65 @@ def main():
 
 @app.route('/add_point', methods=['POST'])
 def add_point():
-    global X, y, fig, ax, w, converged, scatter
+    global is_converged, is_new_point_added, scatter
 
-    converged = False
-
+    is_converged = False
+    is_new_point_added = True
     data = request.get_json()
-    X = np.vstack([X, [1, data['x']]])
-    y = np.vstack([y, data['y']])
+
+    df.loc[len(df)] = [data['x'], data['y']]
 
     if scatter is not None:
         scatter.remove()
-    scatter = ax.scatter(X[:, [1]], y, color='r', s=10)
+    scatter = ax.scatter(df['x'], df['y'], color='r', s=10)
     return fig_to_html(fig)
 
 
 @app.route('/grad_desc', methods=['POST'])
 def gradient_descent():
-    global X, y, fig, ax, w, line, converged, line_x, current_degree
-
-    if X.shape[0] != 0 and not converged:
+    #global X, y, fig, ax, w, line, is_converged, current_degree, is_new_point_added
+    global current_degree, is_new_point_added, X, y, w, is_converged, line, gd_line_x
+    if len(df) != 0:
         new_degree = int(request.get_json()['degree'])
-        if new_degree != current_degree:
+        if new_degree != current_degree or is_new_point_added:
             x_vals = []
             line_x_vals = []
             for i in range(new_degree + 1):
-                x_vals.append(X[:, [1]] ** i)
-                line_x_vals.append(line_x[:, [1]] ** i)
-
+                x_vals.append(df[['x']].to_numpy() ** i)
+                line_x_vals.append(og_line_x_vals ** i)
             X = np.hstack(x_vals)
-            line_x = np.hstack(line_x_vals)
+            y = df['y'].to_numpy().reshape((-1, 1))
+            gd_line_x = np.hstack(line_x_vals)
+
             w = np.zeros((new_degree + 1, 1))
             current_degree = new_degree
+            is_converged = False
+            is_new_point_added = False
 
-        tau = (1 / (np.linalg.norm(X, ord=2) ** 2)) * 0.999
-        for i in range(request.get_json()['num_of_iterations']):
-            if converged:
-                break
-            z = w - tau * X.T @ (X @ w - y)
-            if np.linalg.norm(w - z) < epsilon:
-                converged = True
-            w = z
+        if not is_converged:
+            tau = (1 / (np.linalg.norm(X, ord=2) ** 2)) * 0.999
+            for i in range(request.get_json()['num_of_iterations']):
+                if is_converged:
+                    break
+                z = w - tau * X.T @ (X @ w - y)
+                if np.linalg.norm(w - z) < epsilon:
+                    is_converged = True
+                w = z
 
-        line.pop().remove()
-        line = ax.plot(line_x[:, [1]], line_x @ w, color='b', linewidth=3)
+            line.pop().remove()
+            line = ax.plot(gd_line_x[:, [1]], gd_line_x @ w, color='b', linewidth=3)
+
+    if len(df) == 0:
+        converged_text = "Click to add data!"
+    elif is_converged:
+        converged_text = "Line has converged!"
+    else:
+        converged_text = "Line has not converged!"
     return jsonify({'graph': fig_to_html(fig),
-                    'converged': converged,
-                    'cost': round(cost(), 2),
-                    'intercept': round(w[0].item(), 4),
-                    'slope': round(w[1].item(), 4)})
+                    'converged': converged_text,
+                    'cost': round(cost(), 2)})
+                    # 'intercept': round(w[0].item(), 4),
+                    # 'slope': round(w[1].item(), 4))
 
 
 def cost():
